3d472c4ce0db830147d1b036e008893816996e38
Gabriel Leibel <gfleibel@hotmail.com>
2023-05-25 17:43:50 -0300

    routes, controllers and views ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+html_content = URI.open(url).read
+json_content = JSON.parse(html_content)
+json_content_results = json_content["results"]
+
+puts "Cleaning database..."
+Movie.destroy_all
+
+puts "Creating movies..."
+
+json_content_results.each do |result|
+  Movie.create(title: result["title"], overview: result["overview"], poster_url: "https://image.tmdb.org/t/p/original#{result["poster_path"]}", rating: result["vote_average"])
+end
+
+puts "Created #{Movie.all.count}"
+
+puts "Finished seeding."
+
+
+puts "Now lets seed some lists..."
+puts "Cleaning database..."
+List.destroy_all
+
+puts "Creating lists..."
+
+List.create(name: "Classic" )
+List.create(name: "Sci-Fi" )
+List.create(name: "Suspense/Terror" )
+List.create(name: "Comedy" )
+List.create(name: "Romance" )
+List.create(name: "Action" )
+List.create(name: "Fantasy" )
+
+puts "Created #{List.all.count}"
+
+puts "Finished seeding."
